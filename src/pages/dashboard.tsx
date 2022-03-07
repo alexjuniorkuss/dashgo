fe7c23f14de0fa1ec4b3d53cc6e40fa51707882d
Alex Junior Kuss <alex.kuss@havan.com.br>
2022-03-07 18:18:36 -0300

    ok

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -55,34 +55,27 @@ const series = [
 
 export default function Dashboard() {
     return (
-        <Flex direction="column" h="100vh">
-            <Header />
-
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar />
-
-                <SimpleGrid flex="1" gap="4" minChildWidth="320px">
-                    <Box
-                        p={["6","8"]}
-                        bg="gray.800"
-                        borderRadius={8}
-                        pb="4"
-                    >
-                        <Text fontSize="lg" mb="4">Inscritos da Semana</Text>
-                        <Chart options={options} series={series} type="area" height={160} />
-                    </Box>
-                    <Box
-                        p={["6","8"]}
-                        bg="gray.800"
-                        borderRadius={8}
-                        pb="4"
-                    >
-                        <Text fontSize="lg" mb="4">Taxa de Abertura</Text>
-                        <Chart options={options} series={series} type="area" height={160} />
-
-                    </Box>
-                </SimpleGrid>
-            </Flex>
+      <Flex direction="column" h="100vh">
+        <Header />
+        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+          <Sidebar />
+  
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+            <Box p={["4", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Inscritos da semana
+              </Text>
+              <Chart options={options} series={series} type="area" height={160} />
+            </Box>
+  
+            <Box p={["4", "8"]} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Taxa de Abertura
+              </Text>
+              <Chart options={options} series={series} type="area" height={160} />
+            </Box>
+          </SimpleGrid>
         </Flex>
+      </Flex>
     );
-}
+  }
